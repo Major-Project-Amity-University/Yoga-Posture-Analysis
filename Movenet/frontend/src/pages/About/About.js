de7a633d638a60439ff88a0c853ec05d833e36d2
Sagar Wadhwa <sagarwadhwa726@yahoo.com>
2024-05-21 00:40:00 +0530

    Added movnet algorithm

---
+++ Movenet/frontend/src/pages/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './About.css'
+
+export default function About() {
+    return (
+        <div className="about-container">
+            <h1 className="about-heading">About</h1>
+            <div className="about-main">
+                <p className="about-content">
+                    
+                </p>
+            </div>
+        </div>
+    )
+}
